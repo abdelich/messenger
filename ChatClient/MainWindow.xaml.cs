2c3fe3885329279a2b71a555d0e00f684729f157
abdelich <sarminiabdel@gmail.com>
2023-06-08 12:39:17 +0200

    init

---
+++ ChatClient/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ChatClient.ServiceChat;
+using ClassLibrary1;
+
+namespace ChatClient
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IServiceChatCallback
+    {
+        int ID;
+        bool isConnected = false;
+        ServiceChatClient client;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        void ConnectUser()
+        {
+            if (!isConnected)
+            {
+                client = new ServiceChatClient(new System.ServiceModel.InstanceContext(this));
+
+                ID = client.Connect(tbUsername.Text);
+
+                tbUsername.IsEnabled = false;
+
+                //****
+                lbUsers.Items.Add(tbUsername.Text);
+
+                connectionButton.Content = "Отключиться";
+                isConnected = true;
+            }
+        }
+
+        void DisconnectUser()
+        {
+            if (isConnected) 
+            {
+                client.Disconnect(ID);
+                client = null;
+
+                tbUsername.IsEnabled = true;
+
+                connectionButton.Content = "Подключиться";
+                isConnected = false;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(isConnected)
+            {
+                DisconnectUser();
+            }
+            else
+            {
+                ConnectUser();
+            }
+        }
+
+        public void MsgCallback(string msg)
+        {
+            
+            lbChat.Items.Add(msg);
+            lbChat.ScrollIntoView(lbChat.Items[lbChat.Items.Count-1]);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            DisconnectUser();
+        }
+
+        void SendMessage()
+        {
+            string msg = tbMessage.Text;
+
+            if (!string.IsNullOrEmpty(msg) && client != null)
+            {
+                client.SendMsg(msg,ID);
+                tbMessage.Text = string.Empty;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SendMessage();
+        }
+
+        private void tbMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                SendMessage();
+            }
+        }
+    }
+}
---
+++ ChatHost/Program.cs
@@ -0,0 +1,32 @@
+﻿using ChatClient.ServiceChat;
+using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new ServiceHost(typeof(ServiceChat)))
+            {
+                host.Open();
+                Console.WriteLine("Сервер запущен и работает.");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    class ServerResponse : IServerChatCallback
+    {
+        public void MsgCallback(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
