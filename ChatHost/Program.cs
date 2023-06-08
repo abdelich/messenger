4b3ed68ab703901793f82d5d017bf0b3b54b2fed
abdelich <sarminiabdel@gmail.com>
2023-06-08 13:39:16 +0200

    changed localhost to ipv4

--- ChatHost/Program.cs
+++ ChatHost/Program.cs
@@ -3,6 +3,7 @@ using ClassLibrary1;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
